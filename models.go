324d53a4cd76d4d26d9e96ec0119a1cb23d31c18
Zueff Daniil <70032504+ZueffC@users.noreply.github.com>
2022-02-05 18:23:34 +0300

    Update models.go

--- models.go
+++ models.go
@@ -79,7 +79,7 @@ func connect() {
 	db.AutoMigrate(&AboutUser{})
 	db.AutoMigrate(&Comments{})
 	db.AutoMigrate(&Video{})
-	//MySQLDB.AutoMigrate(&User{})
+	MySQLDB.Migrator().CreateTable(&User{})
 }
 
 func MD5(data string) string {
